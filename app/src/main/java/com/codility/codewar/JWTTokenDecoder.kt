a48757029eba24b9c116e4f8b070dcdb39f341fe
Kelvin Kioko <kelvinmkioko@gmail.com>
2023-01-25 16:07:25 +0300

    Finally figured out that my solution was working but it needed the android environment to be tested

--- app/src/main/java/com/codility/codewar/JWTTokenDecoder.kt
+++ app/src/main/java/com/codility/codewar/JWTTokenDecoder.kt
@@ -1,7 +1,7 @@
 package com.codility.codewar
 
 import android.os.Build
-import com.auth0.android.jwt.JWT
+import androidx.annotation.RequiresApi
 import com.google.gson.Gson
 import kotlinx.coroutines.runBlocking
 import android.util.Base64 as AndroidBase64
@@ -10,57 +10,48 @@ import java.util.Base64 as JavaBase64
 fun main(args: Array<String>) {
     val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMjQ4IiwiY3VzdG9tZXJfbm8iOiI2IiwiY3VzdG9tZXJfbmFtZSI6IkEtWiBNVVJFUkEiLCJzYWxlc19hcmVhcyI6W3siY29kZSI6IjUwMiIsImFyZWEiOiIgTElNVVJVICJ9XSwiY3JlZGl0X2FyZWFzIjpbXSwidXNlcl90eXBlIjoiVVNFUiIsInVzZXJuYW1lIjoiNiIsInBpbiI6IjAiLCJmaXJzdF9uYW1lIjoiQWRtaW4iLCJsYXN0X25hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwibW9iaWxlbm8iOiItIiwidXNlcl9zdGF0dXMiOiJBY3RpdmUiLCJleHBpcmF0aW9uIjoxNjc0NjQ2ODg0LCJpc3N1ZV90aW1lIjoxNjc0NjQwODg0LCJpc3N1ZWRfYnkiOiJQUkVTSEFNQSJ9.bD60IZ0PfOtrpEP0I377NHPwcoFw4SYTOrZrNXUwAzQ"
 
-//    val ceck = decodeToken(jwt = token)
-    randomTest(bodyJWT = token)
-
-//    println("@@@ Check: $ceck")
+    val ceck = decodeToken(token = token)
 }
 
-private fun decodeToken(jwt: String): String {
-    val parts = jwt.split(".")
-    val charset = charset("UTF-8")
+private fun decodeToken(token: String): Any {
+    val parts = token.split(".")
 
-    val header = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        String(JavaBase64.getUrlDecoder().decode(parts[0].toByteArray(charset)), charset)
-    } else {
-        androidBase(bodyJWT = parts[0])
-    }
+    val header = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        javaBase64Decoder(content = parts[0])
+    else
+        androidBaseDecoder(content = parts[0])
 
-    val payload = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        String(JavaBase64.getUrlDecoder().decode(parts[1].toByteArray(charset)), charset)
-    } else {
-        AndroidBase64.decode(parts[0], AndroidBase64.DEFAULT).decodeToString()
-    }
+    val payload = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        javaBase64Decoder(content = parts[1])
+    else
+        androidBaseDecoder(content = parts[1])
 
-    println("@@@ Header: $header")
-    println("@@@ Payload: $payload")
-    val load: TokenPayload = Gson().fromJson(payload, TokenPayload::class.java)
-    println("@@@ Data class: $load")
     return try {
-        payload
+        Gson().fromJson(payload, TokenPayloadModel::class.java)
     } catch (e: Exception) {
         "Error parsing JWT: $e"
     }
 }
 
-private fun androidBase(bodyJWT: String): String {
-    var content = ""
+@RequiresApi(Build.VERSION_CODES.O)
+private fun javaBase64Decoder(content: String): String {
+    val charset = charset("UTF-8")
+    return String(JavaBase64.getUrlDecoder().decode(content.toByteArray(charset)), charset)
+}
+
+private fun androidBaseDecoder(content: String): String {
+    var decodedString: String
     runBlocking {
-        content = AndroidBase64.decode(bodyJWT, AndroidBase64.DEFAULT).decodeToString()
+        decodedString = AndroidBase64.decode(content, AndroidBase64.DEFAULT).decodeToString()
     }
-    return content
+    return decodedString
 }
 
-private fun randomTest(bodyJWT: String) {
-    val jwt = JWT(bodyJWT)
-    println("@@@ ${jwt.header}")
-}
-
-data class TokenPayload(
+data class TokenPayloadModel(
     val user_id: String,
     val customer_no: String,
     val customer_name: String,
-    val sales_areas: List<SalesArea> = emptyList(),
+    val sales_areas: List<SalesAreaModel> = emptyList(),
     val credit_areas: List<Any> = emptyList(),
     val user_type: String,
     val username: String,
@@ -75,7 +66,7 @@ data class TokenPayload(
     val issued_by: String
 )
 
-data class SalesArea(
+data class SalesAreaModel(
     val code: String,
     val area: String
 )
